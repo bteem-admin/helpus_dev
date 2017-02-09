e93a4f15107cd217eed25d1bfb4756629f92c4e1
haseeb736 <haseeb736@gmail.com>
2017-02-09 18:14:30 +0530

    test

--- controllers/Dashboard.php
+++ controllers/Dashboard.php
@@ -27,6 +27,7 @@ class Dashboard extends CI_Controller {
 		$show_data['user_categories']=$this->Generalsettings_model->getall_user_categories($condition);
 
 		$this->load->view('dashboard_view',$show_data);
+		//
 
 	}
 
